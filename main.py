c2bf66b43d1689d99a3044b5d8b95ae8661a2bf3
GodKimba <rafaelfadel2@gmail.com>
2022-04-28 07:04:45 -0300

    refactored the code through creating a user class, functions apparently working, but still need to connect the functionality with the typer library

--- main.py
+++ main.py
@@ -6,39 +6,195 @@ from decouple import config
 from enum import Enum
 import os
 
-# Environment variables to hide the default value from cli
-os.environ["USERNAME"] = config("USERNAME")
-os.environ["PASSWORD"] = config("PASSWORD")
+# # Environment variables to hide the default value from cli
+# os.environ["USERNAME"] = config("USERNAME")
+# os.environ["PASSWORD"] = config("PASSWORD")
 
-# Using envvar for lack of better tool
-def main(
-    user: str = typer.Option(
-        None,
-        envvar=["USERNAME"],
-        show_envvar=False,
-        prompt="Please, insert your mail account",
-    ),
-    password: str = typer.Option(
-        None,
-        envvar=["PASSWORD"],
-        prompt="Please insert your mail password",
-        hide_input=True,
-        show_envvar=False,
-    ),
-):
+# # Using envvar for lack of better tool
+# def main(
+#     user: str = typer.Option(
+#         None,
+#         envvar=["USERNAME"],
+#         show_envvar=False,
+#         prompt="Please, insert your mail account",
+#     ),
+#     password: str = typer.Option(
+#         None,
+#         envvar=["PASSWORD"],
+#         prompt="Please insert your mail password",
+#         hide_input=True,
+#         show_envvar=False,
+#     ),
+# ):
 
-    mail_server = "imap.gmail.com"
-    imap = imaplib.IMAP4_SSL(mail_server)
-    imap.login(user, password)
-    imap.select("inbox")
+#     mail_server = "imap.gmail.com"
+#     imap = imaplib.IMAP4_SSL(mail_server)
+#     imap.login(user, password)
+#     imap.select("inbox")
 
     
-    def delete_and_expunge():
+#     def delete_and_expunge():
+#         imap.expunge()
+#         imap.close()
+#         imap.logout()
+
+#     def delete_by_sender():
+#         sender = input("Enter the sender mail address: ").lower().strip()
+#         status, messages = imap.search(None, f"FROM {sender}")
+#         messages = messages[0].split(b" ")
+#         # Loop to iterate over targeted mails and mark them as deleted
+#         for mail in messages:
+#             _, msg = imap.fetch(mail, "(RFC822)")
+#             # This second loop is only for printing the SUBJECT of targeted mails
+#             for response in msg:
+#                 if isinstance(response, tuple):
+#                     msg = email.message_from_bytes(response[1])
+#                     # Decoding the mail subject
+#                     subject = decode_header(msg["Subject"])[0][0]
+#                     if isinstance(subject, bytes):
+#                         # If it is a bytes type, decode to str
+#                         subject = subject.decode()
+#                     print("Deleting", subject)
+#             # Marking the mail as deleted
+#             imap.store(mail, "+FLAGS", "\\Deleted")
+#         print("Deletion successeful!")
+#         reload_deletion()
+
+
+#     def delete_by_subject():
+#         subject = input("Enter the subject key-words: ").lower().strip()
+#         status, messages = imap.search(None, f"SUBJECT {subject}")
+#         messages = messages[0].split(b" ")
+#         # Loop to iterate over targeted mails and mark them as deleted
+#         for mail in messages:
+#             _, msg = imap.fetch(mail, "(RFC822)")
+#             # This second loop is only for printing the SUBJECT of targeted mails
+#             for response in msg:
+#                 if isinstance(response, tuple):
+#                     msg = email.message_from_bytes(response[1])
+#                     # Decoding the mail subject
+#                     subject = decode_header(msg["Subject"])[0][0]
+#                     if isinstance(subject, bytes):
+#                         # If it is a bytes type, decode to str
+#                         subject = subject.decode()
+#                     print("Deleting", subject)
+#             # Marking the mail as deleted
+#             imap.store(mail, "+FLAGS", "\\Deleted")
+#         print("Deletion successeful!")
+#         reload_deletion()
+
+#     # Need to put all of this inside a function
+#     def deletion_parent():
+#         user_response = (
+#             typer.prompt(
+#                 "Do you wish to search by subject or by sender? (type: subject, sender or quit)"
+#             )
+#             .lower()
+#             .strip()
+#         )
+
+#         if user_response == "sender":
+#             try:
+#                 delete_by_sender()
+
+#             except TypeError:
+#                 print("Try again, be sure that the sender email exists.")
+
+#         elif user_response == "subject":
+#             try:
+#                 delete_by_subject()
+#             except TypeError:
+#                 print("Try again, be sure that the subject key words exists.")
+#         else:
+#             print("Be sure to type a valid answer")
+#             main()
+
+    
+#     def reload_deletion():
+#         user_response = input("Do you wan't to delete more items?(y/n)").lower().strip()
+#         if user_response == "n":
+#             delete_and_expunge()
+
+#         if user_response == "y":
+#             deletion_parent()
+    
+#     deletion_parent()    
+#     delete_and_expunge()
+
+
+# if __name__ == "__main__":
+#     typer.run(main)
+
+
+# # Still need to change the way that the default user is set
+# # Fix the error from searching non existing keys words
+
+
+
+class User:
+    def __init__(self, username, password, mail_server):
+        self.username = username
+        self.password = password
+        self.mail_server = mail_server
+
+        imap = imaplib.IMAP4_SSL(self.mail_server)
+        imap.login(self.username, self.password)
+        imap.select("inbox")
+    
+    def register_user(self):
+        pass
+
+    def initialize_user(self):
+        imap = imaplib.IMAP4_SSL(self.mail_server)
+        imap.login(self.username, self.password)
+        imap.select("inbox")
+
+
+    def delete_and_expunge(self):
         imap.expunge()
         imap.close()
         imap.logout()
 
-    def delete_by_sender():
+
+
+    def reload_deletion(self):
+        user_response = input("Do you wan't to delete more items?(y/n)").lower().strip()
+        if user_response == "n":
+            self.delete_and_expunge()
+
+        if user_response == "y":
+            self.deletion_parent()
+    
+
+    def deletion_parent(self):
+        user_response = (
+            typer.prompt(
+                "Do you wish to search by subject or by sender? (type: subject, sender or quit)"
+            )
+            .lower()
+            .strip()
+        )
+
+        if user_response == "sender":
+            try:
+                self.delete_by_sender()
+
+            except TypeError:
+                print("Try again, be sure that the sender email exists.")
+
+        elif user_response == "subject":
+            try:
+                self.delete_by_subject()
+            except TypeError:
+                print("Try again, be sure that the subject key words exists.")
+        else:
+            print("Be sure to type a valid answer")
+            self.deletion_parent()
+
+
+
+    def delete_by_sender(self):
+        self.initialize_user()
         sender = input("Enter the sender mail address: ").lower().strip()
         status, messages = imap.search(None, f"FROM {sender}")
         messages = messages[0].split(b" ")
@@ -58,10 +214,11 @@ def main(
             # Marking the mail as deleted
             imap.store(mail, "+FLAGS", "\\Deleted")
         print("Deletion successeful!")
-        reload_deletion()
+        self.reload_deletion()
 
 
-    def delete_by_subject():
+    def delete_by_subject(self):
+        self.initialize_user()
         subject = input("Enter the subject key-words: ").lower().strip()
         status, messages = imap.search(None, f"SUBJECT {subject}")
         messages = messages[0].split(b" ")
@@ -81,51 +238,39 @@ def main(
             # Marking the mail as deleted
             imap.store(mail, "+FLAGS", "\\Deleted")
         print("Deletion successeful!")
-        reload_deletion()
-
-    # Need to put all of this inside a function
-    def deletion_parent():
-        user_response = (
-            typer.prompt(
-                "Do you wish to search by subject or by sender? (type: subject, sender or quit)"
-            )
-            .lower()
-            .strip()
-        )
-
-        if user_response == "sender":
-            try:
-                delete_by_sender()
-
-            except TypeError:
-                print("Try again, be sure that the sender email exists.")
-
-        elif user_response == "subject":
-            try:
-                delete_by_subject()
-            except TypeError:
-                print("Try again, be sure that the subject key words exists.")
-        else:
-            print("Be sure to type a valid answer")
-            main()
-
-    
-    def reload_deletion():
-        user_response = input("Do you wan't to delete more items?(y/n)").lower().strip()
-        if user_response == "n":
-            delete_and_expunge()
-
-        if user_response == "y":
-            deletion_parent()
-    
-    deletion_parent()    
-    delete_and_expunge()
+        self.reload_deletion() 
 
 
-if __name__ == "__main__":
-    typer.run(main)
 
 
-# Still need to change the way that the default user is set
-# Fix the error from searching non existing keys words
+
+def main(
+    user: str = typer.Option(
+        None,
+        envvar=["USERNAME"],
+        show_envvar=False,
+        prompt="Please, insert your mail account",
+    ),
+    password: str = typer.Option(
+        None,
+        envvar=["PASSWORD"],
+        prompt="Please insert your mail password",
+        hide_input=True,
+        show_envvar=False,
+    ),
+)
+
+primary_user = User(username = config("USERNAME"), password=config("PASSWORD"), mail_server="imap.gmail.com")
+
+primary_user.initialize_user()
+primary_user.delete_by_sender()
+
+
+
+
+
+
+
+
+
 

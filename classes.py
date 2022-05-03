85862ebfd9fbe69735e8e3789a7d1f85db4a4c43
GodKimba <rafaelfadel2@gmail.com>
2022-05-03 10:18:15 -0300

    used the correct try/except pattern to prevent program from quitting when the inputted words aren't found

--- classes.py
+++ classes.py
@@ -1,8 +1,10 @@
+from os import wait
 import typer
 import imaplib
 import email
 from email.header import decode_header
 from decouple import config
+import sys
 
 class User:
     def __init__(self, username, password, mail_server):
@@ -15,7 +17,7 @@ class User:
         global imap
         imap = imaplib.IMAP4_SSL(self.mail_server)
         imap.login(self.username, self.password)
-        imap.select("inbox")
+        imap.select('"[Gmail]/All Mail"')
 
 
     def delete_and_expunge(self):
@@ -68,48 +70,56 @@ class User:
 
 
     def delete_by_sender(self):
-        self.initialize_user()
         sender = input("Enter the sender mail address: ").lower().strip()
-        status, messages = imap.search(None, f"FROM {sender}")
-        messages = messages[0].split(b" ")
-        # Loop to iterate over targeted mails and mark them as deleted
-        for mail in messages:
-            _, msg = imap.fetch(mail, "(RFC822)")
-            # This second loop is only for printing the SUBJECT of targeted mails
-            for response in msg:
-                if isinstance(response, tuple):
-                    msg = email.message_from_bytes(response[1])
-                    # Decoding the mail subject
-                    subject = decode_header(msg["Subject"])[0][0]
-                    if isinstance(subject, bytes):
-                        # If it is a bytes type, decode to str
-                        subject = subject.decode()
-                    print("Deleting", subject)
-            # Marking the mail as deleted
-            imap.store(mail, "+FLAGS", "\\Deleted")
-        print("Deletion successeful!")
+        try:
+            status, messages = imap.search(None, f"FROM {sender}")
+            messages = messages[0].split(b" ")
+            # Loop to iterate over targeted mails and mark them as deleted
+            for mail in messages:
+                _, msg = imap.fetch(mail, "(RFC822)")
+                # This second loop is only for printing the SUBJECT of targeted mails
+                for response in msg:
+                    if isinstance(response, tuple):
+                        msg = email.message_from_bytes(response[1])
+                        # Decoding the mail subject
+                        subject = decode_header(msg["Subject"])[0][0]
+                        if isinstance(subject, bytes):
+                            # If it is a bytes type, decode to str
+                            subject = subject.decode()
+                        print("Deleting", subject)
+                # Marking the mail as deleted
+                imap.store(mail, "+FLAGS", "\\Deleted")
+            print("Deletion successeful!")
+        except:
+            typer.echo(f"Error, try again.", err=True)
+            self.delete_by_sender()
         self.reload_deletion()
 
 
     def delete_by_subject(self):
-        self.initialize_user()
-        subject = input("Enter the subject key-words: ").lower().strip()
+        subject = input("Enter the subject key-words: ")
         status, messages = imap.search(None, f"SUBJECT {subject}")
-        messages = messages[0].split(b" ")
-        # Loop to iterate over targeted mails and mark them as deleted
-        for mail in messages:
-            _, msg = imap.fetch(mail, "(RFC822)")
-            # This second loop is only for printing the SUBJECT of targeted mails
-            for response in msg:
-                if isinstance(response, tuple):
-                    msg = email.message_from_bytes(response[1])
-                    # Decoding the mail subject
-                    subject = decode_header(msg["Subject"])[0][0]
-                    if isinstance(subject, bytes):
-                        # If it is a bytes type, decode to str
-                        subject = subject.decode()
-                    print("Deleting", subject)
-            # Marking the mail as deleted
-            imap.store(mail, "+FLAGS", "\\Deleted")
-        print("Deletion successeful!")
+        try:
+            messages = messages[0].split(b" ")
+            # Loop to iterate over targeted mails and mark them as deleted
+            for mail in messages:
+                _, msg = imap.fetch(mail, "(RFC822)")
+                # This second loop is only for printing the SUBJECT of targeted mails
+                for response in msg:
+                    if isinstance(response, tuple):
+                        msg = email.message_from_bytes(response[1])
+                        # Decoding the mail subject
+                        subject = decode_header(msg["Subject"])[0][0]
+                        if isinstance(subject, bytes):
+                            # If it is a bytes type, decode to str
+                            subject = subject.decode()
+                        print("Deleting", subject)
+                # Marking the mail as deleted
+                imap.store(mail, "+FLAGS", "\\Deleted")
+            print("Deletion successeful!")
+        except:
+            typer.echo(f"Error, try again.", err=True)
+            self.delete_by_subject()
         self.reload_deletion() 
+
+
